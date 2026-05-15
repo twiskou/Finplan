3bee3e97df06c2beafd9f850e6ccadbd7fa2e08b
Khairo23 <kheireddinedebz@gmail.com>
2026-05-15 22:32:20 +0200

    fix bug 2 deploment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaLibSql } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -10,11 +9,10 @@ function createPrismaClient() {
   const url = process.env.DATABASE_URL || 'file:./dev.db'
   const authToken = process.env.DATABASE_AUTH_TOKEN
 
-  const libsql = createClient({
+  const adapter = new PrismaLibSql({
     url,
     authToken,
   })
-  const adapter = new PrismaLibSql(libsql)
   return new PrismaClient({ adapter } as any)
 }
 
